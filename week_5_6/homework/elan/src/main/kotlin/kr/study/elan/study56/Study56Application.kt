dd52f07f1d34c954e8c989d5d79fd46da4438ef4
hoi9un <hoi9un@gmail.com>
2025-05-18 22:47:13 +0900

    5-6주차 프로젝트 추가

---
+++ week_5_6/homework/elan/src/main/kotlin/kr/study/elan/study56/Study56Application.kt
@@ -0,0 +1,11 @@
+package kr.study.elan.study56
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Study56Application
+
+fun main(args: Array<String>) {
+	runApplication<Study56Application>(*args)
+}
---
+++ week_5_6/homework/elan/src/test/kotlin/kr/study/elan/study56/Study56ApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.study.elan.study56
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Study56ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
